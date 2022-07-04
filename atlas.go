893993456c501588ed4357f79bef66a6be24ca4f
Mihail Stoykov <M.Stoikov@gmail.com>
2022-07-04 10:59:54 +0300

    init

---
+++ atlas.go
@@ -0,0 +1,84 @@
+package atlas
+
+import (
+	"sync"
+)
+
+type Node struct {
+	root    *Node // immutable
+	prev    *Node // immutable
+	linkKey linkKeyType
+
+	links      map[linkKeyType]*Node // mutable
+	linksMutex sync.Mutex            // TODO replace with atomics if possible
+}
+
+type linkKeyType [2]string
+
+func (n *Node) GoTo(key, value string) *Node {
+	if n.linkKey[0] == key && n.linkKey[1] == value {
+		return n
+	}
+	n.linksMutex.Lock()
+	linkKey := [2]string{key, value}
+	result, ok := n.links[linkKey]
+	n.linksMutex.Unlock()
+	if ok {
+		return result
+	}
+	result = n.add(linkKey)
+	return result
+}
+
+func (n *Node) add(linkKey linkKeyType) *Node {
+	if n.linkKey == linkKey {
+		return n
+	}
+	var newNode *Node
+	switch {
+	case n.linkKey[0] == linkKey[0]:
+		// the key is save the value is different
+		// make a new node from the prev
+		newNode = n.prev.add(linkKey)
+	case linkKey[0] < n.linkKey[0] || n.linkKey[0] == "":
+		// we just need to add it here or we at the root
+		newNode = &Node{
+			root:    n.root,
+			prev:    n,
+			linkKey: linkKey,
+			links:   make(map[linkKeyType]*Node),
+		}
+	default:
+		// we need to add this to a previous node in the path to the root
+		// and then add the current node keys on top of that
+		newNode = n.prev.add(linkKey).add(n.linkKey)
+	}
+	// the actual adding
+	n.linksMutex.Lock()
+	if result, ok := n.links[linkKey]; ok {
+		// we raced - no problem just return the old
+		n.linksMutex.Unlock()
+		return result
+	}
+	n.links[linkKey] = newNode
+	n.linksMutex.Unlock()
+	return newNode
+}
+
+func (n *Node) Path() map[string]string {
+	if n.root == n {
+		return make(map[string]string)
+	}
+	result := n.prev.Path()
+	result[n.linkKey[0]] = n.linkKey[1]
+	return result
+}
+
+func New() *Node {
+	n := &Node{
+		links: make(map[linkKeyType]*Node),
+	}
+	n.root = n
+	n.prev = n
+	return n
+}
---
+++ atlas_test.go
@@ -0,0 +1,151 @@
+package atlas
+
+import (
+	"math/rand"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestBasic(t *testing.T) {
+	r := New()
+	n1 := r.GoTo("a", "5")
+	n2 := n1.GoTo("c", "6")
+	n3 := n2.GoTo("b", "7")
+	require.Equal(t, n3.Path(), map[string]string{
+		"a": "5",
+		"b": "7",
+		"c": "6",
+	})
+	require.True(t, r != n1)
+	require.True(t, r != n2)
+	require.True(t, r != n3)
+	require.True(t, n2 != n1)
+	require.True(t, n3 != n1)
+	require.True(t, n2 != n3)
+	require.True(t, n2 == r.GoTo("c", "6").GoTo("a", "5"))
+	require.True(t, n2 == r.GoTo("c", "6").GoTo("a", "5").GoTo("a", "5"))
+}
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+// from https://stackoverflow.com/a/22892986/5427244
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func TestConcurrency(t *testing.T) {
+	// this just test that adding stuff is not racy
+	r := New()
+	values := make([]string, 10000)
+	keys := make([]string, 15)
+	for i := 0; i < len(keys); i++ {
+		keys[i] = randSeq(100)
+	}
+	for i := 0; i < len(values); i++ {
+		values[i] = randSeq(100)
+	}
+	concurrency := 128
+	repetitions := 10240
+	wg := sync.WaitGroup{}
+	for i := 0; i < concurrency; i++ {
+		wg.Add(1)
+
+		go func(j int) {
+			defer wg.Done()
+			n := r
+			for i := 0; i < repetitions; i++ {
+				index := j + i
+				n = n.GoTo(keys[index%len(keys)], values[index%len(keys)])
+			}
+		}(i)
+	}
+	wg.Wait()
+}
+
+func BenchmarkConcurrencyBad(b *testing.B) {
+	values := make([]string, 10000)
+	keys := make([]string, 15)
+	for i := 0; i < len(keys); i++ {
+		keys[i] = randSeq(100)
+	}
+	for i := 0; i < len(values); i++ {
+		values[i] = randSeq(100)
+	}
+	r := New()
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.RunParallel(func(p *testing.PB) {
+		for p.Next() {
+			r.GoTo(
+				keys[rand.Int()%len(keys)],
+				values[rand.Int()%len(values)],
+			).GoTo(
+				keys[rand.Int()%len(keys)],
+				values[rand.Int()%len(values)],
+			).GoTo(
+				keys[rand.Int()%len(keys)],
+				values[rand.Int()%len(values)],
+			).GoTo(
+				keys[rand.Int()%len(keys)],
+				values[rand.Int()%len(values)],
+			).GoTo(
+				keys[rand.Int()%len(keys)],
+				values[rand.Int()%len(values)],
+			).GoTo(
+				keys[rand.Int()%len(keys)],
+				values[rand.Int()%len(values)],
+			).GoTo(
+				keys[rand.Int()%len(keys)],
+				values[rand.Int()%len(values)],
+			)
+		}
+	})
+}
+
+func BenchmarkRealistic(b *testing.B) {
+	values := make([]string, 100000)
+	for i := 0; i < len(values); i++ {
+		values[i] = randSeq(100)
+	}
+	r := New().
+		GoTo("labelone", "valueone").
+		GoTo("labeltthree", "valuetthree").
+		GoTo("labelfour", "valuefour").
+		GoTo("labelfive", "valuefive").
+		GoTo("labelsix", "valuefive").
+		GoTo("labelseven", "valuefive").
+		GoTo("labeleigth", "valuefive").
+		GoTo("labeltwo", "valuetwo")
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.RunParallel(func(p *testing.PB) {
+		i := 0
+		for p.Next() {
+			i++
+			n := r.GoTo(
+				"badkey",
+				values[i%len(values)],
+			)
+			if i%2 == 0 {
+				n = n.GoTo("labelsix", "someOtheStrangeValue")
+			}
+			switch i % 7 {
+			case 0, 1, 2:
+				n.GoTo("okayLabel", "200")
+			case 3, 4:
+				n.GoTo("okayLabel", "400")
+			case 5:
+				n.GoTo("okayLabel", "500")
+			case 6:
+				n.GoTo("okayLabel", "0")
+			}
+
+		}
+	})
+}
